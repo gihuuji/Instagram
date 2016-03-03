4bca9e08dc4b648f7ae07777ae6778dd035bc675
gihuuji <gihuuji@gmail.com>
2016-03-03 23:42:08 +0700

    MyInstagram

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by MacGihuuji on 3/3/2559 BE.
+//  Copyright © 2559 MacGihuuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+ use_frameworks!
+
+target 'Instagram' do
+	pod 'RealmSwift', '~> 0.98'
+	pod 'Firebase', '~> 2.5'
+end
+
+target 'InstagramTests' do
+
+end
+
+target 'InstagramUITests' do
+
+end
+
